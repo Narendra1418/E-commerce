5f9736aae01a3c373fb083e86782ebf1982ff701
Narendra <dnarendra4510@gmail.com>
2026-01-29 14:07:41 +0530

    backend folder structure setup

---
+++ backend/src/test/java/com/shopverse/e_commerce/ECommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopverse.e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
